17a881e003615879f67058356ecb71a87c5064e9
pycxu <pyc.xuu@gmail.com>
2022-05-04 05:39:45 +1000

    Add cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Import packages
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 // Load env variable
 require('dotenv').config();
 // App
 const app = express();
 // Morgan
 app.use(morgan('tiny'));
+// Cors
+app.use(cors());
 // First route
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
